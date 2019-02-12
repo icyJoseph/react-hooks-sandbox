07dbc23dafabf7ea0b4262fb973beca7fcdea461
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-02-12 07:23:35 +0100

    refactor: move top react component to App/

---
+++ src/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>
+            <code>React Hooks</code>
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
